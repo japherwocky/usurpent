0f101ede7696fbd27669bc7f28f6cb22f0f7986f
japherwocky@japhbookpro <github@pearachute.com>
2022-01-15 21:20:47 -0500

    chasing mouse clicks

--- static/game.js
+++ static/game.js
@@ -14,122 +14,138 @@ document.getElementById('cta').addEventListener('click', function(e) {
 
 // set our screen size based on actual browser size
 
-var bounds = {}
 
-function setBounds() {
-
-    bounds.height = screen.availHeight;
-    bounds.width = screen.availWidth;
-
-}
-
-setBounds()
-window.addEventListener('resize', setBounds)
 
 
 
 
 function chart(data) { 
-
-
     window.data = data;
 
-    const zoom = d3.zoom()
-      .on("zoom", zoomed);
+    const svg = d3.select('svg')
+
+    var bounds = {}
+
+    function setBounds() {
+
+        let screen = document.getElementById('screen')
+
+        bounds.height = screen.clientHeight;
+        bounds.width = screen.clientWidth;
+
+    }
+
+    // calculate the bounds of our SVG element
+    setBounds()
+
+    // listen for a browser resize to adjust
+    window.addEventListener('resize', setBounds)
+
 
     const k = bounds.height / bounds.width;
-
     const x = d3.scaleLinear()
-        .domain([-4.5, 4.5])
+        .domain([-3, 3])
         .range([0, bounds.width])
-    window.x = x;
 
     const y = d3.scaleLinear()
-        .domain([-4.5 * k, 4.5 * k])
+        .domain([-3 * k, 3 * k])
         .range([bounds.height, 0])
 
     const z = d3.scaleOrdinal()
-        .domain(data.map(d => d[2]))
+        .domain(data.map(d => d.color))
         .range(d3.schemeCategory10)
 
 
-    const xAxis = (g, x) => g
-        .attr("transform", `translate(0,${bounds.height})`)
-        .call(d3.axisTop(x).ticks(12))
-        .call(g => g.select(".domain").attr("display", "none"))
-
-    const yAxis = (g, y) => g
-        .call(d3.axisRight(y).ticks(12 * k))
-        .call(g => g.select(".domain").attr("display", "none"))    
-
-
-
-    // Our zoom locations ?
-    function mkTransforms(data) {
-        const transforms = [["Overview", d3.zoomIdentity]]  // a list of tuples, the base is the reset zoom function
-
-            .concat(d3.groups(data, d => d[2])  
-                .map(([key, data]) => {
-            const [x0, x1] = d3.extent(data, d => d[0]).map(x);
-            const [y1, y0] = d3.extent(data, d => d[1]).map(y);
-            const k = 0.9 * Math.min(bounds.width / (x1 - x0), bounds.height / (y1 - y0));
-            const tx = (bounds.width - k * (x0 + x1)) / 2;
-            const ty = (bounds.height - k * (y0 + y1)) / 2;
-            return [`Cluster ${key}`, d3.zoomIdentity.translate(tx, ty).scale(k)];
-        }))
+    const gx = svg.append("g")
+        .attr('transform', `translate(0, ${bounds.height -1})`)
+        .call(
+            d3.axisTop(x).ticks(5) 
+        )
+    const gy = svg.append("g")
+        .call(
+            d3.axisRight(y).ticks(3)
+        )                                                                                                       
 
 
-        return transforms;
-    }
-
-    const tform = mkTransforms(data)
-    window.tform = tform;
-
-                                                                                                                 
-
-
-
-
-  const svg = d3.select('svg')
-
-  const g = svg.append("g")
+    // a group of paths for our circles
+    const g = svg.append("g")
       .attr("fill", "none")
       .attr("stroke-linecap", "round");
 
-  g.selectAll("path")
-    .data(data)
-    .join("path")
-      .attr("d", d => `M${x(d[0])},${y(d[1])}h0`)
-      .attr("stroke", d => z(d[2]));
 
-  const gx = svg.append("g");
+    setInterval( function () {
+    // 0 length paths are supposed to be more performant than circles
+        var ents = g.selectAll("path")
+            .data(window.data, (d,i) => i )  // second arg to .data is a function to make a key
 
-  const gy = svg.append("g");
+            // on creation
+        ents.enter()
+            .append('path')
+                .transition().duration(250)
+                .attr("d", d => `M${x(d.x)},${y(d.y)}h0`)
+                .attr("stroke", d => z(d.color))
+                .attr("stroke-width", 13)
+                .attr('opacity', .39)
 
-  // svg.call(zoom.transform, tform);
-  svg.call(zoom.transform, tform[0][1])
 
-  function zoomed(event) {
-    let {transform} = event;
-    debugger;
-    // console.log(transform);
-    g.attr("transform", transform).attr("stroke-width", 5 / k);
-    gx.call(xAxis, transform.rescaleX(x));
-    gy.call(yAxis, transform.rescaleY(y));
-  }
 
-  window.screen = Object.assign(svg.node(), {
-    update(transform) {
-      svg.transition()
-          .duration(1500)
-          .call(zoom.transform, transform);
-    }
-  });
+        // update
+        ents
+            .transition().duration(250)
+            .attr("d", d => `M${x(d.x)},${y(d.y)}h0`)
+            .attr("stroke", d => z(d.id))  // will size change?
+
+
+        // remove
+        ents.exit().remove();
+
+    }, 250)
+
+
+    svg.on('click', function(event,d) {
+
+        pt = [x.invert(event.x), y.invert(event.y)]  // cast our coordinates to model space
+
+        window.data.map( d=> {
+            let delta_x = pt[0] - d.x
+            let delta_y = pt[1] - d.y
+            let dist = Math.sqrt( delta_x**2 + delta_y**2 )
+
+            d.x += delta_x / dist
+            d.y += delta_y / dist
+            return d 
+        })
+        console.log(pt)
+
+    })
+
+
 
 }
 
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+
 function mkData() {
-  const random = d3.randomNormal(0, 0.2);
+
+    const random = d3.randomNormal(0, 1);
+
+    return Array.from({length: 300}, f => {
+        out = {}
+        out.id = uuidv4()
+        out.x = random()
+        out.y = random()
+        out.color = random()
+
+        return out
+    })
+
+
   const sqrt3 = Math.sqrt(3);
   return [].concat(
     Array.from({length: 300}, () => [random() + sqrt3, random() + 1, 0]),
